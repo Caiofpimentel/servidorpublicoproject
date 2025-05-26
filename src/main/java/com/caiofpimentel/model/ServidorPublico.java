605e59b5cecbac0ace951f980aeeada0cc0a8d84
Caio <caio-pimentel@hotmail.com>
2025-05-26 16:03:22 -0300

    Cria Record do Servidor Publico

---
+++ src/main/java/com/caiofpimentel/model/ServidorPublico.java
@@ -0,0 +1,23 @@
+package com.caiofpimentel.model;
+
+public record ServidorPublico( 
+    
+            Long matricula,    
+            String nome,
+            String foto,
+            String orgao,
+            String vinculo,    
+            String cargo,
+            String lotacao,
+            String exercicio,
+            String email,
+            String telefone,
+            String celular,
+            String cpf,
+            String naturalidade) {
+
+
+
+
+
+}
