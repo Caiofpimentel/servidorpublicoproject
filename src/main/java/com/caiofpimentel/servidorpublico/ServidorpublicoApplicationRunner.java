1080b0badf8da7e2aa5820b53eeadfb5a1c56f37
Caio <caio-pimentel@hotmail.com>
2025-05-26 15:56:13 -0300

    Cria a Camada Application

---
+++ src/main/java/com/caiofpimentel/servidorpublico/ServidorpublicoApplicationRunner.java
@@ -0,0 +1,18 @@
+package com.caiofpimentel.servidorpublico;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+
+
+
+@Configuration
+public class ServidorpublicoApplicationRunner implements CommandLineRunner{ 
+
+    @Override
+    public void run(String... args) throws Exception {
+        // This method can be used to execute code after the application has started
+        
+        
+    }
+    
+}
